982053e9db6a82cbf95f90cdafe4d998f84229ef
c0a2419427 <c0a2419427@edu.teu.ac.jp>
2025-04-15 17:19:58 +0900

    演習問題１

--- pygame_image.py
+++ pygame_image.py
@@ -27,9 +27,9 @@ def main():
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((2,0))
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        else:
-            kk_rct.move_ip((-1,0))
+            kk_rct.move_ip((-1,0)) 
+
+        kk_rct.move_ip((-1,0))
            
         x=tmr%3200
      
