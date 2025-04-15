f65f88207c21078b4ec4ac5bbf729c0f5e8049f5
c0a2419427 <c0a2419427@edu.teu.ac.jp>
2025-04-15 15:51:37 +0900

    練習6 こうかとん動きました

--- pygame_image.py
+++ pygame_image.py
@@ -16,13 +16,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            x=-tmr
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [x, 0])#練習６
         screen.blit(kk_img,[300,200])
         
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200)#練習５
 
 
 if __name__ == "__main__":
